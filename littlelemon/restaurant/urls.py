662587d64841cc791ebe9e47abd2b3d0394eb0ed
zeal2211 <nair.harish88@gmail.com>
2024-05-31 11:47:08 -0500

    Used Default Router in projects url file

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
+from rest_framework import viewsets
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def get(self, request, *args, **kwargs):
+        menu_items = self.get_queryset()
+        serializer = self.serializer_class(menu_items, many=True)
+        return Response(serializer.data)
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def get(self, request, pk=None, *args, **kwargs):
+        menu_item = get_object_or_404(Menu, pk=pk)
+        serializer = self.serializer_class(menu_item)
+        return Response(serializer.data)
+
+    def put(self, request, pk=None, *args, **kwargs):
+        menu_item = get_object_or_404(Menu, pk=pk)
+        serializer = self.serializer_class(menu_item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk=None, *args, **kwargs):
+        menu_item = get_object_or_404(Menu, pk=pk)
+        menu_item.delete()
+        return Response({'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer    
+
+    
